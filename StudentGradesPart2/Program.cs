587d91d727250dbcfe2825977cd39f3f8e2f1006
JCuadrado <jcuadrado@psecu.com>
2024-05-29 13:15:08 -0400

    starting first project and added .gitignore

--- StudentGradesPart2/Program.cs
+++ StudentGradesPart2/Program.cs
@@ -2,7 +2,12 @@
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
 
+int examAssignments = 5;
 
 string DetermineLetterGrade(decimal grade)
 {
@@ -70,19 +75,28 @@ void StudentInformation(string studentName, int[] grades)
     foreach (var grade in grades)
     {
         currentAssignments++;
-        totalSum += grade;
+        
+        if (currentAssignments <= examAssignments)
+        {
+            
+        }
     }
+
+    Console.WriteLine(currentAssignments);
 
     score = (decimal)totalSum / currentAssignments;
 
-
-    Console.WriteLine($"{studentName}:\t\t" + score + $"\t{DetermineLetterGrade(score)}");
+    Console.WriteLine($"{studentName}:\t\t" + $"{score}\t\t" + $"'n'\t\t" +  $"{DetermineLetterGrade(score)}" + "\t'n'");
 }
 
-Console.WriteLine("Student\t\tGrade\n");
+Console.WriteLine("Student\t\tExamScore\tOverall\t\tGrade\tExtraCredit\n");
 StudentInformation("Sophia", sophiaScores);
 StudentInformation("Andrew", andrewScores);
 StudentInformation("Emma", emmaScores);
 StudentInformation("Logan", loganScores);
+StudentInformation("Becky", beckyScores);
+StudentInformation("Chris", chrisScores);
+StudentInformation("Eric", ericScores);
+StudentInformation("Gregor", gregorScores);
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
--- StudentGrading/obj/Debug/net8.0/StudentGrading.AssemblyInfo.cs
+++ StudentGrading/obj/Debug/net8.0/StudentGrading.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293f1bc93ab52adfc05fbe86f72d25db838ef14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab2fd4435a8c2cfd40a1ee6cd6517997af94e33")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentGrading")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
